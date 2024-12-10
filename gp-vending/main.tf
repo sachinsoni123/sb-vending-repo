1cb61977189c29944e2f7f1a513779d5b928cb15
sachinsoni123 <164299395+sachinsoni123@users.noreply.github.com>
2024-12-10 10:39:50 +0000

    creating new sb projects

--- gp-vending/main.tf
+++ gp-vending/main.tf
@@ -23,28 +23,28 @@ module "app_folder" {
 }
 
 
-# module "app_project" {
-#   for_each       = local.project_vending_data_map
-#   source         = "../modules/project_factory"
-#   project_name   = each.value.settings.project_name
-#   api            = each.value.settings.api
-#   folder_id      = module.app_folder[each.key].folder_id
-#   labels         = each.value.settings.labels
-#   owners_members = each.value.settings.owners_members
-#   depends_on = [
-#     module.app_folder
-#   ]
-# }
+ module "app_project" {
+   for_each       = local.project_vending_data_map
+   source         = "../modules/project_factory"
+   project_name   = each.value.settings.project_name
+   api            = each.value.settings.api
+   folder_id      = module.app_folder[each.key].folder_id
+   labels         = each.value.settings.labels
+   owners_members = each.value.settings.owners_members
+   depends_on = [
+     module.app_folder
+   ]
+ }
 
-# module "app_budget_alert" {
-#   for_each        = local.project_vending_data_map
-#   source          = "../modules/budget"
-#   project_name    = each.value.settings.project_name
-#   billing_id      = each.value.settings.billing_id
-#   project_no      = module.app_project[each.key].project_number
-#   approved_budget = each.value.settings.approved_budget
-#   members         = each.value.settings.notification_members
-#   depends_on = [
-#     module.app_project
-#   ]
-# }
+ module "app_budget_alert" {
+   for_each        = local.project_vending_data_map
+   source          = "../modules/budget"
+   project_name    = each.value.settings.project_name
+   billing_id      = each.value.settings.billing_id
+   project_no      = module.app_project[each.key].project_number
+   approved_budget = each.value.settings.approved_budget
+   members         = each.value.settings.notification_members
+   depends_on = [
+     module.app_project
+   ]
+ }
