ba818ea2d94b03258d90444cf55ebc92d9946257
sachinsoni123 <164299395+sachinsoni123@users.noreply.github.com>
2024-12-11 16:57:31 +0530

    Update main.tf

--- gp-vending/main.tf
+++ gp-vending/main.tf
@@ -1,5 +1,6 @@
 module "global_practice" {
   for_each        = { for k, v in local.project_vending_data_map : k => v if v.settings.gp-vending != false }
+ # for_each        = local.project_vending_data_map
   source          = "../modules/folders"
   organization_id = each.value.settings.gp-vending.organization_id
   folder_name     = each.value.settings.gp-vending.names_gp
@@ -8,6 +9,7 @@ module "global_practice" {
 
 module "poc" {
   for_each        = { for k, v in local.project_vending_data_map : k => v if v.settings.gp-vending != false }
+# for_each       = local.project_vending_data_map
   source          = "../modules/folders"
   organization_id = each.value.settings.gp-vending.organization_id
   folder_name     = each.value.settings.gp-vending.names_poc
@@ -16,6 +18,7 @@ module "poc" {
 
 module "app_folder" {
   for_each        = { for k, v in local.project_vending_data_map : k => v if v.settings.gp-vending != false }
+# for_each       = local.project_vending_data_map
   source          = "../modules/folders"
   organization_id = each.value.settings.gp-vending.organization_id
   folder_name     = each.value.settings.gp-vending.names_app
@@ -25,6 +28,7 @@ module "app_folder" {
 
 module "app_project" {
   for_each       = { for k, v in local.project_vending_data_map : k => v if v.settings.gp-vending != false }
+# for_each       = local.project_vending_data_map
   source         = "../modules/project_factory"
   project_name   = each.value.settings.gp-vending.project_name
   api            = each.value.settings.gp-vending.api
@@ -39,6 +43,7 @@ module "app_project" {
 
 module "app_budget_alert" {
   for_each        = { for k, v in local.project_vending_data_map : k => v if v.settings.gp-vending != false }
+# for_each       = local.project_vending_data_map
   source          = "../modules/budget"
   project_name    = each.value.settings.gp-vending.project_name
   billing_id      = each.value.settings.gp-vending.billing_id
